27c44975abc44eaecafbbb3c6dbca7396fde99d9
Vahram Jacobs <vahram.jacobs@gmail.com>
2022-08-23 23:03:36 -0700

    adds / after baseUrl

--- client/api/buildAxiosInstance.js
+++ client/api/buildAxiosInstance.js
@@ -5,14 +5,13 @@ export default function buildAxiosInstance({ req }) {
 	const isUsedInNodeJS = typeof window === 'undefined';
 	if (isUsedInNodeJS) {
 		axiosInstance = axios.create({
-			// lalalala
 			baseURL:
 				// 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
 				// [Weren't these requests sent to nginx from our cluster internally,
 				// like above?
 				// Why do we need to send them to an external address?]
 				// [Also, shouldn't this depend on the env, prod or dev?]
-				'http://www.jeenj.com',
+				'http://www.jeenj.com/',
 			headers: req.headers,
 		});
 	} else {
